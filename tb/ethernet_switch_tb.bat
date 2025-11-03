8cea0dd3b476ea139fe8883a513ca0c28b9b5ff0
Mac898 <xmachd@gmail.com>
2025-11-03 23:51:48 +1100

    push latest gtkw

--- tb/ethernet_switch_tb.bat
+++ tb/ethernet_switch_tb.bat
@@ -1,5 +1,5 @@
 call path.bat
-ghdl -a -Paltera/altera_mf/v93 -Paltera/altera_lnsim/v93 -Paltera/cycloneiv/v93 altclkctrl_stub.vhd reset_ctrl_stub.vhd ram_tx_driver.vhd axi4s_interconnect.vhd pll_main.vhd ringbuffer.vhd tx_phy.vhd eth_tx_ram_driver.vhd ram_eth_packet.vhd sr_piso.vhd tx_fsm_pt.vhd tx_fsm_axi.vhd eth_tx.vhd eth_port.vhd ethernet_switch.vhd ethernet_switch_tb.vhd
+ghdl -a -Paltera/altera_mf/v93 -Paltera/altera_lnsim/v93 -Paltera/cycloneiv/v93 tb\altclkctrl_stub.vhd tb\reset_ctrl_stub.vhd axi4s_interconnect.vhd rx_2ff.vhd ram_eth_packet.vhd rx_fcs_crc.vhd rx_decoder.vhd sr_sipo.vhd rx_fsm_pr.vhd rx_fsm_axi.vhd rx_fcs_verify.vhd rx_fsm_adr.vhd eth_rx.vhd pll_main.vhd ringbuffer.vhd tx_phy.vhd sr_piso.vhd tx_fsm_pt.vhd tx_fsm_axi.vhd eth_tx.vhd eth_port.vhd ethernet_switch.vhd tb\ethernet_switch_tb.vhd
 ghdl -e -Paltera/altera_mf/v93 -Paltera/altera_lnsim/v93 -Paltera/cycloneiv/v93 --ieee=synopsys -fexplicit ethernet_switch_tb 
 ghdl -r -Paltera/altera_mf/v93 -Paltera/altera_lnsim/v93 -Paltera/cycloneiv/v93 --ieee=synopsys -fexplicit ethernet_switch_tb --wave=ethernet_switch_tb.ghw
 call gtkwave.bat ethernet_switch_tb.ghw
--- tb/ethernet_switch_tb.vhd
+++ tb/ethernet_switch_tb.vhd
@@ -15,48 +15,55 @@ architecture sim of ethernet_switch_tb is
     signal UART_RX                : std_logic := '1';
     signal UART_TX                : std_logic;
 
+    signal ETH_RX_RAW  : std_logic_vector(4 downto 0) := (others => '1');
     signal ETH_RX      : std_logic_vector(4 downto 0) := (others => '1');
     signal ETH_TX      : std_logic_vector(4 downto 0);
     signal ETH_TX_EN   : std_logic_vector(4 downto 0);
     signal ETH_LED_GRN : std_logic_vector(4 downto 0);
     signal ETH_LED_YEL : std_logic_vector(4 downto 0);
 
-    -- Clock period (adjust as needed)
-    constant CLK_PERIOD : time := 10 ns; -- 50 MHz clock
+    -- Clock periods
+    constant CLK_PERIOD_50MHZ  : time := 20 ns; -- 50 MHz
+    constant CLK_PERIOD_100MHZ : time := 10 ns; -- 100 MHz
 
-    -- 100 MHZ Clock
-    signal clk : std_logic := '0'; -- 100 MHz Clock sync with Main Clock
-    signal resetn : std_logic := '0'; -- reset test signal
-    signal enable : std_logic := '0'; -- enalbe test signal
+    signal clk    : std_logic := '0';   -- 100 MHz clock
+    signal resetn : std_logic := '0';
+    signal enable : std_logic := '0';
 
-    signal test_tvalid : std_logic := '0';
-    signal test_tready : std_logic := '0';
-    signal test_tlast : std_logic := '0';
-    signal test_tdata : std_logic_vector(7 downto 0) := (others => '0');
+    signal test_tvalid : std_logic                    := '0';
+    signal test_tready : std_logic                    := '0';
+    signal test_tlast  : std_logic                    := '0';
+    signal test_tdata  : std_logic_vector(7 downto 0) := (others => '0');
+    signal test_tx_en  : std_logic                    := '0';
 
 begin
+
+    ETH_RX <= not ETH_RX_RAW;
+
+    -------------------------------------------------------------------
     -- Test Units
-    driver_inst: entity work.eth_tx_tb_driver
-        port map (
-            clk => clk,
+    -------------------------------------------------------------------
+    driver_inst : entity work.eth_tx_tb_driver
+        port map(
+            clk    => clk,
             resetn => resetn,
             enable => enable,
             tvalid => test_tvalid,
             tready => test_tready,
-            tlast => test_tlast,
-            tdata => test_tdata
+            tlast  => test_tlast,
+            tdata  => test_tdata
         );
-    
-    eth_tx_inst: entity work.eth_tx
-        port map (
-            clk => clk,
+
+    eth_tx_inst : entity work.eth_tx
+        port map(
+            clk    => clk,
             resetn => resetn,
-            tx => ETH_RX(0),
-            tx_en => '0',
+            tx     => ETH_RX_RAW(0),
+            tx_en  => test_tx_en,
             tvalid => test_tvalid,
             tready => test_tready,
-            tlast => test_tlast,
-            tdata => test_tdata
+            tlast  => test_tlast,
+            tdata  => test_tdata
         );
 
     -------------------------------------------------------------------
@@ -104,15 +111,28 @@ begin
         );
 
     -------------------------------------------------------------------
-    -- Clock generation process
+    -- 50 MHz Main Clock generation
+    -------------------------------------------------------------------
+    main_clk_process : process
+    begin
+        while true loop
+            MAIN_CLK <= '0';
+            wait for CLK_PERIOD_50MHZ / 2;
+            MAIN_CLK <= '1';
+            wait for CLK_PERIOD_50MHZ / 2;
+        end loop;
+    end process;
+
+    -------------------------------------------------------------------
+    -- 100 MHz Clock generation
     -------------------------------------------------------------------
     clk_process : process
     begin
         while true loop
-            MAIN_CLK <= '0';
-            wait for CLK_PERIOD / 2;
-            MAIN_CLK <= '1';
-            wait for CLK_PERIOD / 2;
+            clk <= '0';
+            wait for CLK_PERIOD_100MHZ / 2;
+            clk <= '1';
+            wait for CLK_PERIOD_100MHZ / 2;
         end loop;
     end process;
 
@@ -121,17 +141,20 @@ begin
     -------------------------------------------------------------------
     stim_proc : process
     begin
-        RESET <= '0';
-        KEY4 <= '0';
+        RESET  <= '0';
+        resetn <= '0';
+        KEY4   <= '0';
         wait for 200 ns;
 
-        RESET <= '1';
+        RESET  <= '1';
+        resetn <= '1';
         wait for 10 us;
-        for i in 0 to 9000 loop
+
+        for i in 0 to 5 loop            -- 10,000 times
             enable <= '1';
-            wait for 800 ns;
+            wait for 75 us;
             enable <= '0';
-            wait for 9.6 us;
+            wait for 10 us;
         end loop;
 
         -- Let the system run for a while
