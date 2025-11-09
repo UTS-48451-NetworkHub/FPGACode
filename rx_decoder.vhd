5ad008caadebf4387fee08e16ca81971f7f8daa3
YummyDog <lskjelly@gmail.com>
2025-11-09 19:46:20 +1100

    removed signals for debug

--- rx_decoder.vhd
+++ rx_decoder.vhd
@@ -4,30 +4,19 @@ use IEEE.numeric_std.all;
 
 entity rx_decoder is
   port (
-    clk_in        : in std_logic;
-    manchester_in : in std_logic;
-    resetn        : in std_logic;
-    data_out      : out std_logic := '0';
-    RX_timeout    : out std_logic := '0';
-    bit_valid     : out std_logic := '0'
+    clk_in        : in std_logic;         --! 100 MHz Clock
+    manchester_in : in std_logic;         --! Manchester Input
+    resetn        : in std_logic;         --! Active low reset
+    data_out      : out std_logic := '0'; --! Decoded bitsream
+    RX_timeout    : out std_logic := '0'; --! Timeout for end of packet
+    bit_valid     : out std_logic := '0'  --! Bit is valid and can be read by SIPO
   );
 end rx_decoder;
 
 architecture Behavioral of rx_decoder is
-  signal data_buf        : std_logic_vector(55 downto 0); --!Register to hold incoming edges for clock recovery
+  signal data_buf        : std_logic_vector(55 downto 0);       --!Register to hold incoming edges for clock recovery
   signal manchester_prev : std_logic                    := '0'; --!Prev input to detect edge.
   signal timeout         : std_logic                    := '0'; --!timeout to reset logic
-  signal midcapture      : std_logic                    := '0';
-  signal midcntsig       : std_logic_vector(3 downto 0) := (others => '0');
-
-  attribute keep                     : boolean;
-  attribute keep of midcapture       : signal is true;
-  attribute preserve                 : boolean;
-  attribute preserve of midcapture   : signal is true;
-  attribute mark_debug               : string;
-  attribute mark_debug of midcapture : signal is "true";
-  attribute noprune                  : boolean;
-  attribute noprune of midcapture    : signal is true;
 
 begin
   process (clk_in, resetn)
@@ -42,7 +31,6 @@ begin
       mid_count := (others => '0');
       data_out        <= '0';
       manchester_prev <= '0';
-      midcapture      <= '0';
 
     elsif rising_edge(clk_in) then
       manchester_prev <= manchester_in; --!current manchester input
@@ -55,7 +43,6 @@ begin
         mid_count := (others => '0');
         data_out        <= '0';
         manchester_prev <= '0';
-        midcapture      <= '0';
 
         -- ! Logic to detect preamble
       elsif (data_buf /= x"AAAAAAAAAAAAAA") then
@@ -76,10 +63,9 @@ begin
         --! Logic to decode data after preamble
       else
         --! clock cycles up to mid-bit counted
-        midcapture <= '1';
         if (mid_count < (mid_loc srl 1) + 2) then
           mid_count := mid_count + 1;
-          midcntsig <= std_logic_vector(mid_count);
+  
           bit_valid <= '0';
         elsif (manchester_prev /= manchester_in) then
           --! Data output at mid-bit
@@ -121,5 +107,5 @@ begin
     end if;
   end process;
 
-  RX_timeout <= timeout; --can maybe be simplified.
+  RX_timeout <= timeout; 
 end Behavioral;
